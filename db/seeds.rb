576d54b1994eac2659de102e4bea8c9795901f9d
Rcalaf <rcalaf.cot@gmail.com>
2012-12-25 12:46:46 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(:email => "jcalaf@santpau.cat", :proxy_password => "2412", :proxy_password_confirmation => "2412", :email_confirmation => "jcalaf@santpau.cat", :name => "Joaquim", :last_name => "Calaf", :status => "professor")
-User.create(:email => "jcalaf@santpau.cat", :proxy_password => "2412", :proxy_password_confirmation => "2412", :email_confirmation => "jespinos@santpau.cat", :name => "Juanjo", :last_name => "Espinos", :status => "professor")
+User.create(:email => "jespinos@santpau.cat", :proxy_password => "2412", :proxy_password_confirmation => "2412", :email_confirmation => "jespinos@santpau.cat", :name => "Juanjo", :last_name => "Espinos", :status => "professor")
