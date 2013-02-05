3bfbca5592cfe44264049de24340b62bf11204b3
Rcalaf <rcalaf.cot@gmail.com>
2013-02-05 15:49:46 +0100

    mailer settings

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -2,12 +2,12 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true,
   :address => 'smtp.endoreprocasos.es',
-  :port => 587,
-  #:port => 26,
+  #:port => 587,
+  :port => 26,
   :domain => 'endoreprocasos.es',
-  :authentication => :plain,
-  #:authentication => :login,
-  :user_name => 'info@endoreprocasos.es',
+  #:authentication => :plain,
+  :authentication => :login,
+  :user_name => 'info+endoreprocasos.es',
   :password => 'DgQxXRna5z'
 }
 
