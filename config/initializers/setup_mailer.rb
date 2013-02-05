c4a0f47618d2744a55b3417107aef8f619ad7a4e
Rcalaf <rcalaf.cot@gmail.com>
2013-02-05 16:22:31 +0100

    mailer settings

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,15 +1,15 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,
+  :enable_starttls_auto => false,
   :address => 'smtp.endoreprocasos.es',
   #:address => 'ssl0.ovh.net',
-  #:port => 587,
-  :port => 465,
+  :port => 587,
+  #:port => 465,
   :domain => 'endoreprocasos.es',
   #:authentication => :plain,
   :authentication => :login,
   :user_name => 'info%endoreprocasos.es',
-  :password => 'DgQxXRna5z'
+  :password => 'HSP001'
 }
 
 ActionMailer::Base.default_url_options[:host] = "endoreprocasos.es"
