fc42227d4e64cefa9666180f581835bd2bad9f30
Rcalaf <rcalaf.cot@gmail.com>
2013-04-11 12:58:59 +0200

    bigger files for documentation and some front layout added messages

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,7 +7,7 @@ class Document < ActiveRecord::Base
 
   
   #validates_attachment_content_type :document, :content_type => ["application/pdf","application/zip"], :message => "Solo se admiten archivos PDF o ZIP (español)", :if => :activate_download_validation
-  validates_attachment_size :document, :less_than => 5120000, :message => "Pesado demasiado (5 Mb máximo)"
+  validates_attachment_size :document, :less_than => 6144000, :message => "Pesado demasiado (5 Mb máximo)"
   validates_attachment_presence :document, :message => "Selecciona archivo"
   validates :titol, :presence => {:presence => true,:message => "Escribe un título"}
  # validates :titol, :length => {:maximum => 25,:message => "Titulo: maximo 25ch."}
