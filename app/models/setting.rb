10a479a585bbf1f41d75b9ece9c9790e3a8f41f6
Rcalaf <rcalaf.cot@gmail.com>
2013-11-05 12:25:03 +0100

    minor fixings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,7 +2,7 @@
 class Setting < ActiveRecord::Base
   attr_accessible :footer, :front_footer, :main_title, :subtitle, :footer_logo, :logo, :main_description
   
-  has_attached_file :footer_logo, :styles => { :small => "500x", :big => "800x" }, :convert_options => {:all => ["-strip", "-colorspace RGB"]}
+  has_attached_file :footer_logo, :styles => { :small => "500x", :big => "800x" }, :convert_options => {:all => ["-strip"]}
   has_attached_file :logo, :styles => { :small => "150x" }, :convert_options => {:all => ["-strip", "-colorspace RGB"]}
   
 end
