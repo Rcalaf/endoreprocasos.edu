a8eca8d36a67c73f408ffc1760fd577fc22ecd84
Roger <rcalaf.cot@gmail.com>
2015-11-01 15:53:53 +1100

    fix contact form

--- app/controllers/front/new_controller.rb
+++ app/controllers/front/new_controller.rb
@@ -8,7 +8,6 @@ class Front::NewController < ApplicationController
       if @consulta.valid?
         Mailer.contact(@consulta).deliver
         flash.now[:notice] = "Grácias por contactar con nosotros!"
-        @consulta = Consulta.new
       else
         flash.now[:error] = "Pon un mensaje y un email para enviar el formulario"
       end
